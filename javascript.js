028174834174eaff410a73c54af9a97be41f5bb5
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-02-26 19:02:42 +0000

    Fix uppercase and change to function

--- javascript.js
+++ javascript.js
@@ -16,17 +16,39 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-let playerSelection = prompt('Please enter your choice').toLowerCase();
+let playerSelection = 'paper';
+
+// let playerSelection = prompt('Please enter your choice').toLowerCase();
 
 let computerSelection = getComputerChoice();
 console.log(computerSelection);
 
-if (playerSelection === computerSelection) {
-  alert('It is a tie!');
-} else if ((playerSelection === 'ROCK' && computerSelection === 'PAPER') ||
-  (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') ||
-  (playerSelection === 'SCISSORS' && computerSelection === 'ROCK')) {
-  alert(`You Lose! ${computerSelection} beats ${playerSelection}`);
-} else {
-  alert(`You Win! ${playerSelection} beats ${computerSelection}`);
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
+    if (playerSelection === computerSelection) {
+      return 'It is a tie!';
+    } else if ((playerSelection === 'rock' && computerSelection === 'paper') ||
+      (playerSelection === 'paper' && computerSelection === 'scissors') ||
+      (playerSelection === 'scissors' && computerSelection === 'rock')) {
+      return `You Lose! ${capitalizeFirstLetter(computerSelection)} beats ${capitalizeFirstLetter(playerSelection)}`;
+    } else {
+      return `You Win! ${capitalizeFirstLetter(playerSelection)} beats ${capitalizeFirstLetter(computerSelection)}`;
+    }
+  } else {
+    return 'Not an option';
+  }
 }
+
+console.log(playRound(playerSelection, computerSelection));
+
+// if (playerSelection !== 'rock' || playerSelection !== 'paper' || playerSelection !== 'scissors') {
+//   alert('Not an option');
+// } else if (playerSelection === computerSelection) {
+//   alert('It is a tie!');
+// } else if ((playerSelection === 'ROCK' && computerSelection === 'PAPER') ||
+//   (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') ||
+//   (playerSelection === 'SCISSORS' && computerSelection === 'ROCK')) {
+//   alert(`You Lose! ${capitalizeFirstLetter(computerSelection)} beats ${capitalizeFirstLetter(playerSelection)}`);
+// } else {
+//   alert(`You Win! ${capitalizeFirstLetter(playerSelection)} beats ${capitalizeFirstLetter(computerSelection)}`);
+// }

7b32d5ef9b9f2661fc19fde2ac3ef13fd1743727
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-02-26 18:18:07 +0000

    Make computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-console.log('Hello World!')
+// CREATE list
+// CALL rock paper or scissors
+// INPUT user choice
+// COMPARE both results
+// DISPLAY winner
+
+const options = ['Rock', 'Paper', 'Scissors'];
+
+
+function getComputerChoice(min, max) {
+  let idx = Math.round(Math.random() * (max - min) + min);
+  return options[idx];
+}

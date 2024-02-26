b6b0f8d1cf6c82b858c289b2e4b3626bd80b234a
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-02-26 18:38:02 +0000

    Make game work and change to lowercase

--- javascript.js
+++ javascript.js
@@ -4,14 +4,25 @@
 // COMPARE both results
 // DISPLAY winner
 
-const options = ['ROCK', 'PAPER', 'SCISSORS'];
+const options = ['rock', 'paper', 'scissors'];
 
 
-function getComputerChoice(min, max) {
-  let idx = Math.round(Math.random() * (max - min) + min);
+function getComputerChoice() {
+  let idx = Math.round(Math.random() * (3 - 1));
   return options[idx];
 }
 
-let playerSelection = prompt('Please enter your choice').toUpperCase();
+let playerSelection = prompt('Please enter your choice').toLowerCase();
 
-console.log(playerSelection);
+let computerSelection = getComputerChoice();
+console.log(computerSelection);
+
+if (playerSelection === computerSelection) {
+  alert('It is a tie!');
+} else if ((playerSelection === 'ROCK' && computerSelection === 'PAPER') ||
+  (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') ||
+  (playerSelection === 'SCISSORS' && computerSelection === 'ROCK')) {
+  alert(`You Lose! ${computerSelection} beats ${playerSelection}`);
+} else {
+  alert(`You Win! ${playerSelection} beats ${computerSelection}`);
+}
